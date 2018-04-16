034a9fccc4151e676ab2e8c2f4693cb436d8df6c
Sonu <sonukn089@gmail.com>
2018-04-17 00:20:42 +0530

    print php

---
+++ display_5times.php
@@ -0,0 +1,22 @@
+<?php 
+
+	echo "1. Welcome to php"."<br>";
+	echo "2. Welcome to php"."<br>";
+	echo "3. Welcome to php"."<br>";
+	echo "4. Welcome to php"."<br>";
+	echo "5. Welcome to php<br/>";
+
+/*	echo "P P"  ."H"    ."H" . "P P<br/>";
+	echo "P   P"."H"    ."H" . "P   P<br/>"; 
+	echo "P P"  ."H"."H"."H".  "P P";
+	echo "P"    ."H"    ."H".  "P";
+	echo "P"    ."H"    ."H".  "P";*/
+
+	echo "P P&nbsp;&nbsp&nbsp;"."H&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"    ."H&nbsp;" . "&nbsp;P P<br/>";
+	echo "P&nbsp;&nbsp;&nbsp;P&nbsp;"."H&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."H&nbsp;&nbsp;"."P&nbsp;&nbsp;&nbsp;"."P<br/>"; 
+	echo "P P&nbsp;&nbsp;"."&nbsp;H"."&nbsp;H&nbsp;"."H&nbsp;&nbsp;"."P P<br/>";
+	echo "P&nbsp;&nbsp;&nbsp;&nbsp;"."&nbsp;&nbsp;H&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"    ."H&nbsp;&nbsp;"."P<br/>";
+	echo "P&nbsp;&nbsp;&nbsp;&nbsp;"."H"    ."H".  "P";	
+
+
+ ?>
