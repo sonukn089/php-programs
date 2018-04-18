6231025c7a0e24d4fb8ea8704778c675349a83c8
Sonu <sonukn089@gmail.com>
2018-04-18 22:55:09 +0530

    create form

---
+++ process1.php
@@ -0,0 +1,10 @@
+<?php
+
+	echo "Name: ".$_POST['name']."</br>";
+	echo "email: ".$_POST['email']."</br>";
+	echo "adress: ".$_POST['adress']."</br>";
+	echo "password: ".$_POST['password']."</br>";
+	echo "cpassword: ".$_POST['cpassword'];
+
+?>
+
