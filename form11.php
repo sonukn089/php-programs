93c390208f785ccadc87174492bd7c5dcf2b8b3d
Sonu <sonukn089@gmail.com>
2018-04-19 00:10:15 +0530

    update all files

---
+++ form11.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>GET POST</title>
+</head>
+<body>
+
+	<form method="POST" action="processform.php">
+		
+		<table>
+			<tr>
+				<td>Name:</td>
+				<td></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td></td>
+			</tr>
+		</table>
+
+	</form>
+
+</body>
+</html>
--- process1.php
+++ process1.php
@@ -6,23 +6,23 @@
 	// echo "password: ".$_POST['password']."</br>";
 	// echo "cpassword: ".$_POST['cpassword'];
 	
-	if ($_POST('name')) {
+	if ($_POST['name']) {
 		
-		if ($_POST('email')) {
+		if ($_POST['email']) {
 			
-			if ($_POST('adress')) {
+			if ($_POST['adress']) {
 				
-				if ($_POST('password')) {
+				if ($_POST['password']) {
 					
-					if ($_POST('cpassword')) {
+					if ($_POST['cpassword']) {
 						
-						if ( $_POST('password') == $_POST('cpassword') ) {
+						if ( $_POST['password'] == $_POST['cpassword'] ) {
 							
 							echo "Your form has been sent successfully";
 						}
 						else{
 
-							echo "Your password doesn't match!";
+							echo "Your passwords don't match!";
 						}
 					}
 					else{
