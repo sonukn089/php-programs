71b9e716c7e84169f645af4d25ad551b9ce900f2
Sonu <sonukn089@gmail.com>
2018-04-18 23:11:28 +0530

    process.php

--- process1.php
+++ process1.php
@@ -1,10 +1,30 @@
 <?php
 
-	echo "Name: ".$_POST['name']."</br>";
-	echo "email: ".$_POST['email']."</br>";
-	echo "adress: ".$_POST['adress']."</br>";
-	echo "password: ".$_POST['password']."</br>";
-	echo "cpassword: ".$_POST['cpassword'];
+	// echo "Name: ".$_POST['name']."</br>";
+	// echo "email: ".$_POST['email']."</br>";
+	// echo "adress: ".$_POST['adress']."</br>";
+	// echo "password: ".$_POST['password']."</br>";
+	// echo "cpassword: ".$_POST['cpassword'];
+	
+	if ($_POST('name')) {
+		
+		if ($_POST('email')) {
+			
+			if ($_POST('adress')) {
+				
+				if ($_POST('password')) {
+					
+					if ($_POST('cpassword')) {
+						
+						if ( $_POST('password') == $_POST('cpassword') ) {
+							
+							echo "Your form has been sent successfully";
+						}
+					}
+				}
+			}
+		}
+	}
 
 ?>
 
