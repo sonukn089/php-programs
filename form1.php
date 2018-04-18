f175cd2c51625fd42686b8a698e34dd059173bf6
Sonu <sonukn089@gmail.com>
2018-04-18 22:46:30 +0530

    form1.php

---
+++ form1.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>FORM1</title>
+</head>
+<body>
+
+	<form method="POST" action="process1.php">
+		<table>
+			<tr>
+				<td>Name:</td>
+				<td><input type="text" name="name"></td>
+			</tr>
+			<tr>
+				<td>Email:</td>
+				<td><input type="text" name="email"></td>
+			</tr>
+			<tr>
+				<td>Address:</td>
+				<td><input type="text" name="adress"></td>
+			</tr>
+			<tr>
+				<td>Password:</td>
+				<td><input type="password" name="password"></td>
+			</tr>
+			<tr>
+				<td>Confirm Password:</td>
+				<td><input type="password" name="cpassword"></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><input type="submit" name="submit" value="submit"></td>
+			</tr>
+		</table>
+
+	</form>
+
+</body>
+</html>
