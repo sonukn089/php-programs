eb1fa94c6f07dfc4f3e6d4b77540faf8f96eb3fd
Sonu <sonukn089@gmail.com>
2018-04-20 00:03:06 +0530

    create new form

---
+++ form111.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CRUD</title>
+</head>
+<body>
+	<form method="POST" action="insert.php">
+		<table>
+			<tr>
+				<td>Name:</td>
+				<td><input type="text" name="name"></td>
+			</tr>
+			<tr>
+				<td>Email:</td>
+				<td><input type="text" name="emial"></td>
+			</tr>
+			<tr>
+				<td>Password:</td>
+				<td><input type="password" name="password"></td>
+			</tr>
+			<tr>
+				<td><input type="submit" value="register"></td>
+				<td><input type="reset" value="reset"></td>
+			</tr>
+		</table>
+	</form>
+		
+</body>
+
+</html>
---
+++ insert.php
@@ -0,0 +1,18 @@
+<?php
+
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+
+	if ($name && $email && $password) {
+	
+			
+
+	}
+	else{
+
+
+	}
+
+
+?>
