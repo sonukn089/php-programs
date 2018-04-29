0ecc44214f7e80fee5133b4b3ad543ed40ea6633
Sonu <sonukn089@gmail.com>
2018-04-30 00:09:13 +0530

    add something

--- userform.php
+++ userform.php
@@ -4,6 +4,31 @@
 	<title>User Form</title>
 </head>
 <body>
+	<form>
+		<table>
+			<tr>
+				<td>Name:</td>
+				<td><input type="text" name="name"></td>
+			</tr>
+			<tr>
+				<td>Name:</td>
+				<td><input type="text" name="name"></td>
+			</tr>
+			<tr>
+				<td>Name:</td>
+				<td><input type="text" name="name"></td>
+			</tr>
+			<tr>
+				<td>Name:</td>
+				<td><input type="text" name="name"></td>
+			</tr>
+			<tr>
+				<td>Name:</td>
+				<td><input type="text" name="name"></td>
+			</tr>	
+			
+		</table>
+	</form>
 
 </body>
 </html>
