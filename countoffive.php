69eb6721656347409d7b3b89d77b1589d35bab0b
Sonu <sonukn089@gmail.com>
2018-05-01 15:23:12 +0530

    creat a function of count five

--- countoffive.php
+++ countoffive.php
@@ -2,24 +2,22 @@
 
 	function countFive($five) {
 
-		$n = array(20,5,7,10,5,25,5,50,54,20,5,7,10,5,25,5,50,54,20,30);
+		$n = array(20,5,7,35,5,25,10,50,5,20,5,7,10,5,25,5,50,5,5,30);
 
 		$five = 0;
 
-		for ($i=0; $i<=20; $i++) { 
+		for ($i=0; $i<20; $i++) { 
 			
+			//echo $n[$i]."<br/>";
+
 			if ($n[$i] == 5) {
-				
+			
 				$five++;
-
-				echo $five;
-					
-			}
-			else{
-
-				echo "Not found of number five";
+				
 			}
 		}
+		echo $five;
+
 	}
 	countFive(20);
 
